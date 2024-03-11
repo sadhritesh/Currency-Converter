bc889e8abf9b7f21a60bd1388cdcea39f7f153b2
Ritesh Sadh <sadhritesh32@gmail.com>
2024-03-11 15:20:29 +0530

    API error resolved

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -5,12 +5,12 @@ function useFetchData(currency){
     const [data, setData] = useState({})
 
     useEffect(()=>{
-        let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
 
         fetch(url)
         .then(res=>res.json())
         .then(res=>{
-            // console.log(res);
+            console.log(res);
             setData(res[currency])
         })
         .catch(error=>console.log("Error :",error))
